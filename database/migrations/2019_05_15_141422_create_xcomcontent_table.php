efb142e85744efa77cc46075dbb88bc8d1b27faf
ellie <yijv2013@gmail.com>
2019-05-16 00:17:11 +1000

    create new migration file

---
+++ database/migrations/2019_05_15_141422_create_xcomcontent_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateXcomcontentTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('xcomcontent', function(Blueprint $table)
+		{
+			$table->integer('id', true);
+			$table->text('category', 65535);
+			$table->text('content', 65535);
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('xcomcontent');
+	}
+
+}
