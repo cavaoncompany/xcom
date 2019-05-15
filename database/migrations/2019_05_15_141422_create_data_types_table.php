294ee855d102e55613874fcfbd48ed9cb76f985f
ellie <yijv2013@gmail.com>
2019-05-16 09:25:40 +1000

    change the table name

--- database/migrations/2019_05_15_141422_create_data_types_table.php
+++ database/migrations/2019_05_15_141422_create_data_types_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateDataTypeTable extends Migration {
+class DataTypesCreateTables extends Migration {
 
 	/**
 	 * Run the migrations.
