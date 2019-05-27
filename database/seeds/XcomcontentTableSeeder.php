069e8278f10dff83301cf3ddcf165e7bbd923fe0
ellie <yijv2013@gmail.com>
2019-05-28 03:21:10 +1000

    change the database frame and do some changes of  the homepage to fit the database

--- database/seeds/XcomcontentTableSeeder.php
+++ database/seeds/XcomcontentTableSeeder.php
@@ -20,111 +20,76 @@ class XcomcontentTableSeeder extends Seeder
             0 => 
             array (
                 'id' => 1,
-                'category' => 'PhoneNumber',
-                'content' => '+61430429687',
+                'category' => 'Header',
+                'content' => json_encode(['phoneNumber'=>'+61430429687',
+                                           'phoneIcon'=>'http://localhost/cms/public/storage/XCOMHomePage/phoneicon.png',
+                                          'logoImg'=>'http://localhost/cms/public/storage/XCOMHomePage/xcomlogo.png',
+                                          'logoUrl'=>'#']),                      
+                'section' => 'info',
             ),
+            
             1 => 
             array (
                 'id' => 2,
-                'category' => 'MainTitle',
-                'content' => 'Experienced IT Specialists',
+                'category' => 'HeroBanner',
+                'content' => json_encode(['HeroTitle'=>'Experienced IT Specialists',
+                                          'HeroContents'=>'With over 15 years of experience, we provide the resources & stability necessary for you to focus your efforts on where they belong.',
+                                          'HeroButton'=>'Go Started Now',
+                                          'HeroButtonUrl'=>'#contact',
+                                          'HeroImage'=>'http://localhost/cms/public/storage/XCOMHomePage/xcommain.png']),  
+                'section' => 'info',
             ),
+     
             2 => 
             array (
-                'id' => 4,
-                'category' => 'MainDescription',
-                'content' => 'With over 15 years of experience, we provide the resources & stability necessary for you to focus your efforts on where they belong.',
-            ),
-            3 => 
-            array (
-                'id' => 5,
+                'id' => 3,
                 'category' => 'ServiceTitle',
-                'content' => 'Our Services',
+                'content' => json_encode(['ServiceTitle'=>'Our Services']),
+                'section' => 'service',
             ),
+            3 => [
+                 'id' => 4,
+                 'category' => 'ServiceDetails',
+                 'content' =>json_encode([['name'=>'01',
+                              'title'=>'Email Hosting and Security',
+                              'description'=>'Not really up to speed with your IT Infrastructure we can audit your infrastructure and propose technology upgrades that suite your needs and budget. This can be scoped and applied in transitional stages to limit downtime and maximize up-time.'
+                            ],
+                            ['name'=>'02',
+                              'title'=>'Support Desk',
+                              'description'=>'Remote and onsite support from our Sydney based Engineers with guaranteed response and resolution times.'
+                            ],
+                            ['name'=>'03',
+                              'title'=>'File Sharing',
+                              'description'=>'Your data is secure and available across any device. Share files within your organization or externally with a few clicks.'
+                            ],
+                            ['name'=>'04',
+                              'title'=>'Business Continuity and Disaster Recovery',
+                              'description'=>'We provide the tools and assurance that your data is protected and recoverable. 24 x 7 monitoring and management of your networks and servers whether they’re in the cloud or in your office.'
+                            ]]),
+                'section' => 'service',
+            ],
+            
             4 => 
             array (
-                'id' => 6,
-                'category' => 'FirstServiceTitle',
-                'content' => 'Email Hosting and Security',
+                'id' => 5,
+                'category' => 'about',
+                'content' => json_encode(['description'=>'XCOM understands small business because we too are a small business. XCOM Small Business IT Support solutions provide the right people, technology and best practice to deliver an experience which is tailored to small businesses and their budget. Xcom strive to provide simplified and robust IT solutions for all business models. If you are a new and growing company or a well-established business out team can maintain or integrate a system that is Taylor made to your needs.',
+                                          'imageUrl'=>'http://localhost/cms/public/storage/XCOMHomePage/xcomabout.png'
+                                          ]),
+                'section' => 'about',
             ),
+        
             5 => 
             array (
-                'id' => 7,
-                'category' => 'FirstServiceDescription',
-                'content' => 'Not really up to speed with your IT Infrastructure we can audit your infrastructure and propose technology upgrades that suite your needs and budget. This can be scoped and applied in transitional stages to limit downtime and maximize up-time.',
+                'id' => 6,
+                'category' => 'Footer',
+                'content' => json_encode(['FooterCopyright'=>'Copyright © 2019 Xcom IT Solution. All rights reserved.',
+                'footerLogo'=>'http://localhost/cms/public/storage/XCOMHomePage/xcomlogo.png',
+                'footerLogoUrl'=>'http://localhost'
+                ]),
+                'section' => 'info',
             ),
-            6 => 
-            array (
-                'id' => 8,
-                'category' => 'SecondServiceTitle',
-                'content' => 'Support Desk',
-            ),
-            7 => 
-            array (
-                'id' => 9,
-                'category' => 'SecondServiceDescription',
-                'content' => 'Remote and onsite support from our Sydney based Engineers with guaranteed response and resolution times.',
-            ),
-            8 => 
-            array (
-                'id' => 10,
-                'category' => 'ThirdServiceTitle',
-                'content' => 'File Sharing',
-            ),
-            9 => 
-            array (
-                'id' => 11,
-                'category' => 'ThirdServiceDescription',
-                'content' => 'Your data is secure and available across any device. Share files within your organization or externally with a few clicks.',
-            ),
-            10 => 
-            array (
-                'id' => 12,
-                'category' => 'FourthServiceTitle',
-                'content' => 'Business Continuity and Disaster Recovery',
-            ),
-            11 => 
-            array (
-                'id' => 13,
-                'category' => 'FourthServiceDescription',
-                'content' => 'We provide the tools and assurance that your data is protected and recoverable. 24 x 7 monitoring and management of your networks and servers whether they’re in the cloud or in your office.',
-            ),
-            12 => 
-            array (
-                'id' => 14,
-                'category' => 'AboutDescription',
-                'content' => 'XCOM understands small business because we too are a small business. XCOM Small Business IT Support solutions provide the right people, technology and best practice to deliver an experience which is tailored to small businesses and their budget. Xcom strive to provide simplified and robust IT solutions for all business models. If you are a new and growing company or a well-established business out team can maintain or integrate a system that is Taylor made to your needs.',
-            ),
-            13 => 
-            array (
-                'id' => 15,
-                'category' => 'FooterCopyright',
-                'content' => 'Copyright © 2019 Xcom IT Solution. All rights reserved.',
-            ),
-            14 => 
-            array (
-                'id' => 16,
-                'category' => 'MainImageUrl',
-                'content' => 'http://localhost/cms/public/storage/XCOMHomePage/xcommain.png',
-            ),
-            15 => 
-            array (
-                'id' => 17,
-                'category' => 'AboutBackgroundImageUrl',
-                'content' => 'http://localhost/cms/public/storage/XCOMHomePage/xcomabout.png',
-            ),
-            16 => 
-            array (
-                'id' => 18,
-                'category' => 'LOGOImageUrl',
-                'content' => 'http://localhost/cms/public/storage/XCOMHomePage/xcomlogo.png',
-            ),
-            17 => 
-            array (
-                'id' => 19,
-                'category' => 'PhoneIconImageUrl',
-                'content' => 'http://localhost/cms/public/storage/XCOMHomePage/phoneicon.png',
-            ),
+   
         ));
         
         
